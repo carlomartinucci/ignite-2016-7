7f1f77a6c6ea540d738a3b80be28af09b8cb222c
carlomartinucci <carlo.martinucci@uqido.com>
2016-06-22 10:13:51 +0200

    init repo

---
+++ Readme.md
@@ -0,0 +1,33 @@
+Spiegami: una piattaforma online per esplorare e sviluppare ragionamenti
+
+La struttura base di un’argomento è composta da tre elementi: premesse, implicazione, conclusione. Le premesse e la conclusione sono come dei blocchi, l’implicazione è come una freccia che collega i blocchi. Ogni blocco usato come conclusione può, a sua volta, essere una premessa di un altro argomento. Ripetendo questa struttura si possono formare ragionamenti sempre più complessi, con molti livelli di profondità, ma nei quali ogni passaggio è spiegato da argomenti specifici per quegli elementi coinvolti.
+
+Spiegami vuole essere simile a Wikipedia, ma plurale: per ogni tesi possono esserci strutture argomentative diverse.
+Spiegami vuole essere un network come Facebook, ma meno dispersivo perché basato sui contenuti e sui ragionamenti, e non sulle emozioni e sui battibecchi.
+
+Blocco:
+Una affermazione. Può essere usato come premessa o come conclusione.
+
+Implicazione:
+È una freccia, il collegamento tra un certo numero di blocchi iniziali (premesse) e un blocco finale (conclusione). Spiega perché se le premesse sono vere allora anche la conclusione lo dovrebbe essere.
+
+Argomento:
+È composto da tre elementi: un insieme di blocchi dette premesse, un singolo blocco detto conclusione, e un’implicazione tra quelle premesse e la conclusione.
+
+Ragionamento:
+Ogni ragionamento ha come obiettivo la dimostrazione di una singola tesi. Un ragionamento si sviluppa in una struttura ad albero su un numero arbitrario di livelli.
+Il primo livello è costituito da uno o più argomenti, ciascuno dei quali ha come conclusione la tesi che si vuole dimostrare. Il secondo livello è costituito da argomenti che hanno come conclusioni le premesse del primo livello. E così via.
+Il ragionamento più semplice possibile è costituito da un singolo argomento con una sola premessa.
+
+Obiezione:
+Perché un argomento sia corretto, è necessario che le premesse siano valide e che l’implicazione sia valida. In questo caso, l’argomento mostra la validità della conclusione.
+Per invalidare un argomento si può sollevare un’obiezione ad una delle premesse oppure un’obiezione all’implicazione.
+Un’obiezione ad una premessa è, di fatto, un ragionamento che ha come tesi l’affermazione che nega la premessa. A sua volta questo ragionamento si compone di argomenti, e può ospitare obiezioni eccetera.
+Un’obiezione ad una implicazione invece consiste in un commento che evidenza o la mancanza di una premessa o un errore logico formale.
+
+Utente:
+Chi usa Spiegami. Può creare blocchi, implicazioni, argomenti, ragionamenti, obiezioni.
+Può creare variazioni di ragionamenti esistenti, usando solo alcune degli argomenti presenti.
+Può esprimere opinioni su blocchi, implicazioni e obiezioni (verde scuro: è vero; verde chiaro: forse è vero; grigio: non so; rosso chiaro: forse è falso; rosso scuro: è falso; bianco: ancora nessuna opinione).
+Può impostare i livelli di privacy (a chi mostrare le proprie opinioni).
+Spiegami gli mostra ragionamenti o argomenti che presume gli interessino; o perché contengono elementi sui quali ha mostrato di essere d’accordo, o perché evidenziano potenziali contraddizioni sulle sue preferenze, o perché sono stati fatti o sottoscritti da altri utenti con i quali ha simili opinioni.
---
+++ js/ignite.js
@@ -0,0 +1,290 @@
+$(function () {
+  $('.progress').hide();
+  $("#videodiv").hide();
+  setBackground(1, "#preload");
+  var vid = document.getElementById("videomp4");
+  vid.currentTime = 9 ;
+});
+
+function setBackground(slideNumber, of) {
+  if (of == null) {of = "#content";}
+  $(of).css("backgroundImage", "url(img/"+slideNumber+".jpg)")
+}
+
+function startIgnite() {
+  $('.progress').show();
+  $('#actual-slide').html("" + 1 );
+  setBackground(1);
+  setBackground(2, "#preload");
+  content = $("#"+1).html();
+  $("#change-content").html(content);
+  progressBar(15000, 200);
+  $('#content').css("cursor", "none");
+  var vid = document.getElementById("videomp4");
+  vid.play();
+  $("#videodiv").show();
+  $('#content').css("width", "50vw");
+  $('#content').css("height", "50vh");
+  $('#content').css("bottom", "1vh");
+  $('#content').css("right", "1vh");
+  $('#content p').css("font-size", "10vh");
+  $('#content p').css("padding", "0 3vh");
+
+  // $("#videodiv").fadeIn();
+}
+
+function endIgnite() {
+  $('.progress').hide();
+  $('#actual-slide').html('<span style="color: white">Carlo Martinucci</span> <i id="flame" class="fa fa-fire" style="color: rgba(255,83,13,0.8); transition: color 0.5s ease"></i>');
+  alternateFlame1();
+  $('#actual-slide').css('padding', "1vh 2vh");
+  setBackground(21);
+  content = $("#tnx").html();
+  $('#content').css("background-color", setColor('asd'));
+  $("#content").html(content);
+  setTimeout(function(){
+    $('#content').animate({
+        opacity: 0,
+      }, 5000, function() {
+        $('#content').css("cursor", "auto");
+         // Animation complete.
+      });
+  }, 2000)
+}
+
+function progressBar(T, t) {
+  var actualSlide = 1;
+  var percentage = 0;
+  var color = 0;
+  var delta_percentage = t/T;
+  function advanceProgressBar(dp){
+    setTimeout(function(){
+      percentage += dp;
+      var newVal = 3.10 * percentage * $(window).width() / 10;
+      $('.progress-bar').css("width", newVal);
+      //$('.progress-bar').html("" + Math.round(percentage*T/1000) );
+      if (percentage>=0.98){
+        actualSlide += 1;
+        $('#actual-slide').html("" + actualSlide );
+        setBackground(actualSlide);
+        setBackground(actualSlide+1, "#preload");
+        content = $("#"+actualSlide).html();
+        $("#change-content").html(content);
+        $('#content p').css("font-size", "10vh");
+        $('#content p').css("padding", "0 3vh");
+        animateSlide(actualSlide);
+        percentage = 0;
+        color += 50505;
+        console.log(color)
+        $('.progress-bar').css("background-color", setColor(actualSlide));
+      }
+      if (actualSlide <= 20 ) {
+        advanceProgressBar(dp);
+      }
+      else {
+        endIgnite();
+      }
+    }, t)
+  }
+  advanceProgressBar(delta_percentage);
+}
+
+function setColor(slideNumber) {
+  var color = ""
+  switch(slideNumber) {
+    case 2:
+      color = "rgba(255,0,0,0.7)"
+      break;
+    case 3:
+      color = "rgba(255, 115, 132,0.7)"
+      break;
+    case 5:
+      color = "rgba(255,0,0,0.7)"
+      break;
+    case 6:
+      color = "rgba(10,10,123,0.7)"
+      break
+    case 7:
+      color = "rgba(10,123,10,0.7)"
+      break;
+    case 11:
+      color = "rgba(20,133,133,0.7)"
+      break;
+    case 13:
+      color = "rgba(255,255,10,0.7)"
+      break;
+    case 14:
+      color = "rgba(255,255,10,0.7)"
+      break;
+    case 15:
+      color = "rgba(255,255,10,0.7)"
+      break;
+    case 18:
+      color = "rgba(123,10,123,0.7)"
+      break;
+    case 20:
+      color = "rgba(255,255,10,0.7)"
+      break;
+    default:
+      color = "#333"
+  }
+  return color
+}
+
+function animateSlide(slideNumber) {
+  switch(slideNumber) {
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4444:
+      setTimeout(function(){
+        $('.slide4volume').removeClass("fa-volume-up").addClass("fa-volume-down");
+        setTimeout(function(){
+          $('.slide4volume').removeClass("fa-volume-down").addClass("fa-volume-off");
+        }, 1000);
+      }, 8000);
+      break;
+    case 5:
+      break;
+    case 6:
+      setTimeout(function(){
+          $('.change-color').css("color", "rgba(123,10,123,0.7)")
+          $('.progress-bar').css("background-color", "rgba(123,10,123,0.7)")
+        }, 10000);
+      break;
+    case 7:
+      break;
+    case 8:
+      break;
+    case 9:
+      break;
+    case 10:
+      setTimeout(function(){
+        $('.sitetree').append("<br><i class='fa fa-sitemap'></i><i class='fa fa-sitemap'></i>")
+        setTimeout(function(){
+          $('.sitetree').append("<br><i class='fa fa-sitemap'></i><i class='fa fa-sitemap'></i><i class='fa fa-sitemap'></i><i class='fa fa-sitemap'></i>")
+        }, 1000);
+      }, 8000);
+      break;
+    case 11:
+      setTimeout(function(){
+        $('.duplicate').append(
+          '<br><i style="color: rgba(10,60,123,0.7); transition: color 1s;" class="dup1 fa fa-square-o"></i> ' +
+          '<i style="color: rgba(10,123,10,0.7); transition: color 1s;" class="dup2 fa fa-square-o"></i> ' +
+          '<i class="fa fa-arrow-right"></i> ' +
+          '<i style="color: rgba(20,133,133,0.7)" class="fa fa-square"></i> '
+        );
+        setTimeout(function(){
+          $('.dup1').css("color", "rgba(10,0,183,0.7)");
+          $('.dup2').css("color", "rgba(10,183,0,0.7)");
+        }, 1000)
+      }, 8000);
+      break;
+    case 12:
+      break;
+    case 13:
+      break;
+    case 14:
+      break;
+    case 15:
+      break;
+    case 16:
+      wikiSlide()
+      break;
+    case 17:
+      break;
+    case 18:
+      break;
+    case 19:
+      setTimeout(function(){
+        $('.battery').removeClass("fa-battery-0").addClass("fa-battery-1");
+        setTimeout(function(){
+          $('.battery').removeClass("fa-battery-1").addClass("fa-battery-2");
+          setTimeout(function(){
+            $('.battery').removeClass("fa-battery-2").addClass("fa-battery-3");
+            setTimeout(function(){
+              $('.battery').removeClass("fa-battery-3").addClass("fa-battery-4");
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 10000);
+      break;
+    case 20:
+      break;
+    case 21:
+      break;
+  }
+}
+
+function wikiSlide() {
+  var content = ""
+  for (i=0;i<3;i++){
+    for (j=0;j<3;j++){
+      if ((i==1 && j==2)  ) {
+        content += "<i class='alternate-yellow fa fa-pencil' style='color: rgba(123,123,10,0.7); transition: color 0.5s ease;'></i> "
+      } else if ( i>0 && i<2 && j>0 && j<2 ) {
+        content += "<i class='fa fa-eye' style='opacity: 0;'></i> "
+      } else {
+        content += "<i class='fa fa-eye'></i> "
+      }
+    }
+    content += "<br>"
+  }
+
+  $('.wikislide').append(content);
+  alternateYellow1();
+}
+
+function wikiSlide2() {
+  var content = ""
+  for (i=0;i<20;i++){
+    for (j=0;j<20;j++){
+      if ((i==17 && j==6) || (i==16 && j==18) || (i==10 && j==4)) {
+        content += "<i class='alternate-yellow fa fa-pencil' style='color: rgba(123,123,10,0.7); transition: color 0.5s ease;'></i> "
+      } else if ( i>7 && i<12 && j>6 && j<13 ) {
+        content += "<i class='fa fa-eye' style='opacity: 0;'></i> "
+      } else {
+        content += "<i class='fa fa-eye'></i> "
+      }
+    }
+    content += "<br>"
+  }
+
+  $('.wikislide').append(content);
+  alternateYellow1();
+}
+
+function alternateYellow1 () {
+  $('.alternate-yellow').css('color', 'rgba(123,123,10,0.7)');
+  setTimeout(function(){
+    if($('.alternate-yellow').length > 3) {
+      alternateYellow2();
+    }
+  }, 500)
+}
+
+function alternateYellow2 () {
+  $('.alternate-yellow').css('color', 'rgba(163,163,10,0.7)')
+  setTimeout(function(){
+    if($('.alternate-yellow').length > 3) {
+      alternateYellow1();
+    }
+  }, 500)
+}
+
+function alternateFlame1 () {
+  $('#flame').css('color', 'rgba(255,83,13,0.6)')
+  setTimeout(function(){
+    alternateFlame2();
+  }, 1000)
+}
+
+function alternateFlame2 () {
+  $('#flame').css('color', 'rgba(255,83,13,1)')
+  setTimeout(function(){
+    alternateFlame1();
+  }, 200)
+}
